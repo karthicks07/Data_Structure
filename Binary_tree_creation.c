7bdc9cc90800facf0d9b8162dd6ef79715d3237a
Karthick_S <113781335+karthicks07@users.noreply.github.com>
2023-09-04 19:30:36 +0530

    Add files via upload

---
+++ Binary_tree_creation.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *left,*right;
+}*root=NULL,*temp,*newnode,*a[50];
+void create_tree(){
+    int i=-1,j=-1,flag=0,c=1;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    scanf("%d",&newnode->data);
+    newnode->left=NULL;
+    newnode->right=NULL;
+    i++;
+    a[i]=newnode;
+    if(root==NULL){
+        j++;
+        root=newnode;
+    }
+    while(c){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        scanf("%d",&newnode->data);
+        newnode->left=NULL;
+        newnode->right=NULL;
+        i++;
+        a[i]=newnode;
+        if(flag==0){
+            temp=a[j];
+            temp->left=newnode;
+            flag=1;
+        }
+        else if(flag==1){
+            temp=a[j];
+            temp->right=newnode;
+            flag=0;
+            j++;
+        }
+        printf("Press 1 to continue\nPress 0 to exit\n");
+        scanf("%d",&c);
+        if(c==0)
+        break;
+    }
+}
+void postorder(struct node *w){
+    if(w!=NULL){
+        postorder(w->left);
+        postorder(w->right);
+        printf("%d ",w->data);
+    }
+}
+int main(){
+    create_tree();
+    postorder(root);
+    
+}
