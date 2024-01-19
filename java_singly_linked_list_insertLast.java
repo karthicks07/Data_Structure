4f3a57f5f7b1200513413c9ee3f534f13c56e645
Karthick_Senthil <113781335+karthicks07@users.noreply.github.com>
2024-01-19 14:01:23 +0530

    Create java_singly_linked_list_insertLast.java

---
+++ java_singly_linked_list_insertLast.java
@@ -0,0 +1,94 @@
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+
+    public Node(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+}
+class LinkedList
+{
+    Node head;
+
+    public LinkedList()
+    {
+        this.head=null;
+    }
+    
+    public void append(int data)
+    {
+        Node newnode=new Node(data);
+        if(head==null)
+        {
+            this.head=newnode;
+            System.out.println("Node inserted.\n");
+        }
+        else
+        {
+            Node lastnode=this.head;
+            while(lastnode.next!=null)
+            {
+                lastnode=lastnode.next;
+            }
+            lastnode.next=newnode;
+            System.out.println("Node inserted.\n");
+        }
+    }
+    
+    public void insertLast(int data)
+    {
+        Node newnode=new Node(data);
+        if(this.head==null)
+        {
+            this.head=newnode;
+            System.out.println("Node inserted at last.\n");
+        }
+        else
+        {
+            Node current=this.head;
+            while(current.next!=null)
+            {
+                current=current.next;
+            }
+            current.next=newnode;
+            newnode.next=null;
+            System.out.println("Node inserted at last.\n");
+        }
+    }
+    
+    public void printList()
+        {
+            Node current=this.head;
+            if(this.head==null)
+            {
+                System.out.println("List is empty.\n");
+            }
+            System.out.print("List elements --> ");
+            while(current!=null)
+            {
+                System.out.print(current.data+" ");
+                current=current.next;
+            }
+            System.out.println("\n");
+        }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        LinkedList l=new LinkedList();
+        l.append(1);
+        l.append(2);
+        l.append(3);
+        l.insertLast(4);
+        l.insertLast(5);
+        l.insertLast(6);
+        l.printList();
+    }
+}
